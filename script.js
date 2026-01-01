3dc6a92311dd6e0d3dcf0905fdf5e0ea0df00140
Howard Chen <194877133+howardshowered@users.noreply.github.com>
2025-12-31 21:20:50 -0800

    added icon for weather type"

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ let searchButton = document.querySelector('#search-button');
 
 async function fetchWeather(location) {
     let locationEncoded = encodeURI(location);
-    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationEncoded}?key=${apiKey}`;
-    let response = await fetch(url);
-    let data = await response.json();
-    return await data;
+    try 
+    {
+        let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationEncoded}?key=${apiKey}`;
+        let response = await fetch(url);
+        let data = await response.json();
+        return await data;
+    } catch (error) {
+        console.error("Failed to fetch:", error.message);
+    }
+
 }
 
 class WeatherData {
@@ -36,9 +42,50 @@ function displayWeather( data ) {
 
     let location = document.createElement("h3");
     location.textContent = `Results for: ${data.address}`;
+    let dayOfWeekHeading = document.createElement("h2");
+    console.log(data.days[0]);
+    const [year, month, day] = data.days[0].datetime.split("-");
+    let date = new Date(year, month - 1, day);
+    console.log(date);
+
+    switch (date.getDay()) {
+        case 0:
+            dayOfWeekHeading.textContent = 'Sunday';
+            break;
+        case 1:
+            dayOfWeekHeading.textContent = 'Monday';
+            break;
+        case 2:
+            dayOfWeekHeading.textContent = "Tuesday";
+            break;
+        case 3:
+            dayOfWeekHeading.textContent = "Wednesday";
+            break;
+        case 4:
+            dayOfWeekHeading.textContent = "Thursday";
+            break;
+        case 5:
+            dayOfWeekHeading.textContent = "Friday";
+            break;
+        case 6:
+            dayOfWeekHeading.textContent = "Saturday";
+            break;      
+    }
     let temp = document.createElement("h2");
+
+
     temp.textContent = `Temp now: ${data.days[0].temp}`;
     weatherCard.appendChild(location);
+    let icon = document.createElement('img');
+    if(data.days[0].icon === 'rain') {
+        icon.src = "./res/rain.svg";
+       
+    } else if (data.days[0].icon === 'snow') {
+        icon.src = "./res/snow.svg";
+
+    }
+    weatherCard.appendChild(icon);
+    weatherCard.appendChild(dayOfWeekHeading);
     weatherCard.appendChild(temp);
 
 } 
